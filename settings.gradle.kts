9919aeaa8ab5b5768b3e0717e29193fc533ef0c1
Kysska_ <saifutdinovaila514@gmail.com>
2025-05-31 20:45:06 +1000

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,27 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "meal-planners-app"
+include(":app")
+include(":data:recipe")
+include(":data:product")
+include(":data:category")
+include(":data:mealtime")
